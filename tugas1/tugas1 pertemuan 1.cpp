0651508a51bd0772014b45c854f30db306f96954
unknown <nazwaazahrarahman@gmail.com>
2023-10-20 11:24:39 +0700

    first commit

---
+++ tugas1/tugas1 pertemuan 1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("nama saya : Nazwa Azahra Rahman, \n");
+	printf(" nim \t : 2341041. \n");
+	printf(" jurusan : Teknik Informatika. \n");
+	
+	return 0;
+}
